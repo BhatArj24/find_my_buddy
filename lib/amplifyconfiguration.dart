a8258ac9e968797e57f42e97b186ecad7380bcc6
Arjun Bhat <arjun19mi@gmail.com>
2024-10-12 21:12:33 -0700

    added

--- lib/amplifyconfiguration.dart
+++ lib/amplifyconfiguration.dart
@@ -1,33 +1,53 @@
-const amplifyconfig = ''' {
-  "UserAgent": "aws-amplify-cli/2.0",
-  "Version": "1.0",
-  "auth": {
-    "plugins": {
-      "awsCognitoAuthPlugin": {
-        "IdentityManager": {
-          "Default": {}
-        },
-        "CognitoUserPool": {
-          "Default": {
-            "PoolId": "us-west-2_FXZMZ8y6o",
-            "AppClientId": "6nv5losg6ba9kf48h5la2of2po",
-            "Region": "us-west-2"
-          }
-        },
-        "Auth": {
-          "Default": {
-            "authenticationFlowType": "USER_SRP_AUTH",
-            "usernameAttributes": ["email"],
-            "signupAttributes": [
-              "email", "name"
-             ],
-            "passwordProtectionSettings": {
-                "passwordPolicyMinLength": 8,
-                "passwordPolicyCharacters": []
+const amplifyconfig = '''{
+    "UserAgent": "aws-amplify-cli/2.0",
+    "Version": "1.0",
+    "auth": {
+        "plugins": {
+            "awsCognitoAuthPlugin": {
+                "UserAgent": "aws-amplify-cli/0.1.0",
+                "Version": "0.1.0",
+                "IdentityManager": {
+                    "Default": {}
+                },
+                "CredentialsProvider": {
+                    "CognitoIdentity": {
+                        "Default": {
+                            "PoolId": "us-west-2:3512ac74-a08a-4950-b97f-02a42a9e7a25",
+                            "Region": "us-west-2"
+                        }
+                    }
+                },
+                "CognitoUserPool": {
+                    "Default": {
+                        "PoolId": "us-west-2_RJLrMm9dq",
+                        "AppClientId": "5hegk5k440gabtjfe7o9i4j2pl",
+                        "Region": "us-west-2"
+                    }
+                },
+                "Auth": {
+                    "Default": {
+                        "authenticationFlowType": "USER_SRP_AUTH",
+                        "socialProviders": [],
+                        "usernameAttributes": [
+                            "EMAIL"
+                        ],
+                        "signupAttributes": [
+                            "EMAIL", "NAME"
+                        ],
+                        "passwordProtectionSettings": {
+                            "passwordPolicyMinLength": 8,
+                            "passwordPolicyCharacters": []
+                        },
+                        "mfaConfiguration": "OFF",
+                        "mfaTypes": [
+                            "SMS"
+                        ],
+                        "verificationMechanisms": [
+                            "EMAIL"
+                        ]
+                    }
+                }
             }
-          }
         }
-      }
     }
-  }
 }''';
